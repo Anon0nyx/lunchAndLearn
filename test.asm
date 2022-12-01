815d2b436dae4113bd059f6e0af067c29ef0fc4b
unknown <dylantforkey@gmail.com>
2022-12-01 16:14:13 -0500

    working on global variables and accessing them

--- test.asm
+++ test.asm
@@ -1,6 +1,9 @@
 .386
 .MODEL flat
 
+.DATA
+	foo DW 12
+
 .CODE
 _main PROC
 
@@ -12,15 +15,28 @@ _main PROC
 	
 	foobar:
 		; Function start
-		push ebp ; -4 from ESP
+		push ebp ; -4 from ESP every time we push, +4 when pop
 		mov ebp, esp ; Saving our stack pointer for when we need to leave the function
-		sub esp, 4 ; making room for one 4-byte local variable
+		sub esp, 10h ; making room for four 4-byte local variable, this variable will be referenced
+		             ; by ebp because ebp holds the original stack, these 16-bytes are now before 
+					 ; any stack that could be created during this function. If we want to access it we
+					 ; do so by referencing [ebp-index], for example [ebp-8] will hold the second variable
+					 ; [ebp-C] will hold the second, and so on
 		push ebx ; This variable is modified within the function so we want to save it
 		push ecx ; Same with this variable
 		
-		mov ebx, [ebp+8]
+		
+		mov ebx, [ebp+8h] ; ebp becomes our reference to paramaters passed as it holds the
+		                  ; original stack pointer address when we entered the function
+						  ; Here we are moving the value passed into the stack into ebx
 		mov ecx, ebx
-		mov [ebp-4], ecx ; Making use of that blank space in the stack
+		mov [ebp-4h], ecx ; Making use of the first blank space in the stack
+		mov ecx, 2h
+		mov ebx, 3h
+		imul ecx, ebx
+		mov [ebp-8h], ecx ; Using the second blank stack slot to store local value
+		push [foo]
+		pop [ebp-12]
 		
 		; Cleanup
 		pop ecx ; Putting variables back for function exit
@@ -34,4 +50,3 @@ _main PROC
 _main ENDP
 
 END _main
-	
